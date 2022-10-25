4a18661e1f0c97590a4a60a628b6921f91ab5c9c
tracy sikenjana <unboxedcanvas@gmail.com>
2022-10-25 17:00:12 +0200

    Completed upto create method

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post '/tasks', to: 'tasks#create'
+  #Update a restaurant
+  #   # See the EDIT form for the restaurant
+  # get '/restaurants/:id/edit', to: "restaurants#edit"
+  #   # PATCH the restaurant with the new info
+  # patch '/restaurants/:id', to: "restaurants#update"
 end
